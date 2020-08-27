7f8cf6ed45d92363c1b0700411044e1f8688d01c
Luiz Eduardo Satelis dos Santos <edusatelis@pop-os.localdomain>
2020-08-27 18:37:41 -0300

    subindo o final da seção 2

--- exercicios.php
+++ exercicios.php
@@ -2,13 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="assets\css\style.css">
-    <link rel="stylesheet" href="assets\css\exercicios.css">
-
-    <title>Exercicios</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exercicios.css">
+    <title>Exercício</title>
 </head>
 <body class="exercicio">
     <header class="cabecalho">
@@ -16,15 +13,19 @@
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href="#" class="verde" >Sem formatação</a>
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
+            class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
     </nav>
-    <main>
+    <main class="principal">
         <div class="conteudo">
-
+            <?php
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
         </div>
     </main>
     <footer class="rodape">
-        COD3R & ALUNOS © <?= date('Y') ?>
+        COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,119 +1,30 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="assets\css\style.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Curso PHP</title>
 </head>
-
 <body>
     <header class="cabecalho">
         <h1>Curso PHP</h1>
-        <h2>Índice dos Exercicios</h2>
+        <h2>Índice dos Exercícios</h2>
     </header>
     <main class="principal">
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01 - Básico</h3>
+                    <h3>Módulo 01</h3>
                     <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-
+                        <li><a href="exercicios.php?dir=teste&file=teste">Exercício A</a></li>
                     </ul>
                 </div>
-                <div class="modulo vermelho">
-
-                    <h3>Módulo 02 - Básico</h3>
-                    <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-
-                    </ul>
-                </div>
-                <div class="modulo azul">
-                    <h3>Módulo 03 - Básico</h3>
-                    <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-
-                    </ul>
-
-                </div>
-                <div class="modulo roxo">
-
-                    <h3>Módulo 04 - Básico</h3>
-                    <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-
-                    </ul>
-
-                </div>
-                <div class="modulo laranja">
-                    <h3>Módulo 05 - Básico</h3>
-                    <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-    
-                    </ul>
-
-                </div>
-                <div class="modulo verde-escuro">
-                    
-                    <h3>Módulo 06 - Básico</h3>
-                    <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-    
-                    </ul>
-                </div>
-                <div class="modulo vermelho-escuro">
-
-                    <h3>Módulo 07 - Básico</h3>
-                    <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-    
-                    </ul>
-                </div>
-                <div class="modulo azul-escuro">
-
-                    <h3>Módulo 08 - Básico</h3>
-                    <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-    
-                    </ul>
-                </div>
-                <div class="modulo roxo-escuro">
-
-                    <h3>Módulo 09 - Básico</h3>
-                    <ul>
-                        <li><a href="#">Exercicio A</a></li>
-                        <li><a href="#">Exercicio B</a></li>
-                        <li><a href="#">Exercicio C</a></li>
-    
-                    </ul>
-                </div>
-        </div>
-        </nav>
+            </nav>
         </div>
     </main>
     <footer class="rodape">
-        COD3R & ALUNOS © <?= date('Y') ?>
+        COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 </body>
-
 </html>
