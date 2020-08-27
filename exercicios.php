63b62bb690a6a25cb34fde5fd13de72298b9f35b
Luiz Eduardo Satelis dos Santos <edusatelis@pop-os.localdomain>
2020-08-27 17:06:53 -0300

    first commit

---
+++ exercicios.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="assets\css\exercicios.css">
+
+    <title>Exercicios</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde" >Sem formatação</a>
+    </nav>
+    <main>
+        <div class="conteudo">
+
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y') ?>
+    </footer>
+</body>
+</html>
